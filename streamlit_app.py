a0933abbcf7bade5120e818a79cbd819b2a10b69
ketut <ketut@bps.go.id>
2024-02-10 23:26:25 +0800

    add bonus

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,23 +6,7 @@ st.title('EfPeeL - Likelihood FC')
 
 df_data = pd.read_csv('merged_gw_23.csv')
 
-pemainku = [
-    'Jordan Pickford',
-    'Oleksandr Zinchenko',
-    'William Saliba',
-    'Vitalii Mykolenko',
-    'Cole Palmer',
-    'Richarlison de Andrade',
-    'Kevin De Bruyne',
-    'Bukayo Saka',
-    'Julián Álvarez',
-    'Ollie Watkins',
-    'Dominic Solanke',
-    'Guglielmo Vicario',
-    'Virgil van Dijk',
-    'Pedro Porro',
-    'Anthony Gordon'
-]
+
 nama_tim = df_data['team'].unique()
 
 kolom1, kolom2 = st.columns(2)
@@ -41,7 +25,11 @@ with kolom1:
 
     player_data1 = df_data1[df_data1['name'] == selected_pemain1]
     st.line_chart(
-    player_data1, x='GW', y=[['total_points','bonus']], color=["#FF0000"]  # Optional
+    player_data1, x='GW', y='total_points', color=["#FF0000"]  # Optional
+    )
+    st.write(f'Bonus per gameweek - {selected_pemain1}')
+    st.line_chart(
+    player_data1, x='GW', y='bonus', color=["#0000FF"]
     )
 
 with kolom2:
@@ -58,5 +46,9 @@ with kolom2:
     st.write(f"{selected_tim2} >> {selected_pemain2}")
     player_data2 = df_data2[df_data2['name'] == selected_pemain2]
     st.line_chart(
-    player_data2, x='GW', y=[['total_points', 'bonus']], color=["#0000FF"]
+    player_data2, x='GW', y='total_points', color=["#0000FF"]
+    )
+    st.write(f'Bonus per gameweek - {selected_pemain2}')
+    st.line_chart(
+    player_data2, x='GW', y='bonus', color=["#0000FF"]
     )
