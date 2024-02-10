21e3f1ccc96acbe0e8850aa2a1d9e1e5836fa52d
ketut <ketut@bps.go.id>
2024-02-10 21:49:19 +0800

    add selectbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,31 @@ import pandas as pd
 import numpy as np
 
 st.title('EfPeeL - Likelihood FC')
+
+df_data = pd.read_csv('merged_gw_23.csv')
+pemainku = [
+    'Jordan Pickford',
+    'Oleksandr Zinchenko',
+    'William Saliba',
+    'Vitalii Mykolenko',
+    'Cole Palmer',
+    'Richarlison de Andrade',
+    'Kevin De Bruyne',
+    'Bukayo Saka',
+    'Julián Álvarez',
+    'Ollie Watkins',
+    'Dominic Solanke',
+    'Guglielmo Vicario',
+    'Virgil van Dijk',
+    'Pedro Porro',
+    'Anthony Gordon'
+]
+
+option = st.selectbox(
+    'Select player1?',
+    [pemain: pemain in pemainku]    
+    )
+
+st.write('You selected:', option)
+
+
