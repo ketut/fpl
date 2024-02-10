3e3198e25cf7785d4ba5348c8566750d41acef8b
ketut <ketut@bps.go.id>
2024-02-11 00:05:22 +0800

    add subheader with divider

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ nama_tim = df_data['team'].unique()
 
 liga = requests.get('https://fantasy.premierleague.com/api/leagues-classic/2381820/standings/').json()
 df_liga = pd.DataFrame(liga['standings']['results'])
-st.title(liga['league']['name'])
+st.subheader(liga['league']['name'], divider=grey)
 st.table(df_liga[['entry_name','rank','last_rank','total']])
 
 
