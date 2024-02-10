cc9bbf0b360faf2e35b45330483140f63d4445eb
ketut <ketut@bps.go.id>
2024-02-10 22:20:53 +0800

    add 2nd selectbox

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import numpy as np
 st.title('EfPeeL - Likelihood FC')
 
 df_data = pd.read_csv('merged_gw_23.csv')
+
 pemainku = [
     'Jordan Pickford',
     'Oleksandr Zinchenko',
@@ -23,11 +24,22 @@ pemainku = [
     'Anthony Gordon'
 ]
 
-selected_pemain = st.selectbox(
+selected_pemain1 = st.selectbox(
     'Select player1?',
      pemainku    
     )
 
-st.write('You selected:', selected_pemain)
+st.write('You selected:', selected_pemain1)
 
+selected_pemain2 = st.selectbox(
+    'Select player2?',
+     pemainku    
+    )
+st.write('You selected:', selected_pemain2)
 
+player_data1 = df_data[df_data['name'] == selected_pemain1]
+player_data2 = df_data[df_data['name'] == selected_pemain2]
+gameweek = player_data1['GW']
+st.line_chart(
+   chart_data, x=gameweek, y=[player_data1['total_points'], player_data2['total_points']], color=["#FF0000", "#0000FF"]  # Optional
+)
