f09ea8ca4074aa6287c8d00e33d5c7bc0b84438f
ketut <ketut@bps.go.id>
2024-02-11 13:51:44 +0800

    add tim skuad

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ for baris in liga_bps5100['standings']['results']:
         id_tim = daftar_tim[selected_tim_bps5100]
         st.success(id_tim)
         team = requests.get(f"https://fantasy.premierleague.com/api/entry/{id_tim}/event/{selected_gameweek}/picks/").json()
-        if team['detail'] == "Not found.":
+        if team['detail']:
             st.warning("Pilih gameweek yang sesuai")
         else:
             players = [x['element'] for x in team['picks']]
