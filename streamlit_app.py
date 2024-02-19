b4b3e0665347f34c26e00cfc0eabea33bcae5f59
artayasa <artayasa@gmail.com>
2024-02-19 14:08:12 +0000

    edit read_csv filename

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import requests
 st.title('EfPeeL - Likelihood FC')
 st.subheader('Ngutang kandik, nuduk sanshaw', divider='grey')
 
-df_data = pd.read_csv('merged_gw_24.csv')
+df_data = pd.read_csv('merged_gw.csv')
 df_data = df_data.sort_values(by='total_points', ascending=False)
 nama_tim = df_data['team'].unique()
 players =  requests.get('https://fantasy.premierleague.com/api/bootstrap-static/').json()
